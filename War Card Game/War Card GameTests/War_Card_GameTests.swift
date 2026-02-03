8633e193805ab2220eb2e4ed5020ee156752793a
Robert Palmer <Awriter247@gmail.com>
2026-02-03 17:55:02 -0500

    Initial Commit

---
+++ War Card Game/War Card GameTests/War_Card_GameTests.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameTests.swift
+//  War Card GameTests
+//
+//  Created by Robert Palmer on 2/3/26.
+//
+
+import Testing
+@testable import War_Card_Game
+
+struct War_Card_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
